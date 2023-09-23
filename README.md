9a363713a4473a56987bc2812bac01cfeffa6881
Jordan Lee <jplee01@wm.edu>
2023-09-23 17:08:57 -0400

    Added gdbtest.c
    Needed to commit to move local dir

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# CSCI304Lab1
+# CSCI 304 Lab 1
 Lab 1 Project in C for CSCI304 - Computer Organization
+
+Name: **Jordan Lee**
+
+Time Spent on Lab (Est.): TBD
+
+Comments, Concerns, Etc.
+- 
---
+++ gdbtest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+
+    int i = 2;
+    while (i < 256){
+        i = square(i);
+    }
+
+    printf(("%d\n", i));
+    return (EXIT_SUCCESS);
+}
+int square(int x){
+    int y = x * x;
+    return (x * x);
+}
